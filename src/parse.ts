e2303ab4a536197a96852f8bfa3c59f4b3053bd3
Antony Bartlett <antony.bartlett@ukri.org>
2022-08-06 22:55:47 +0100

    Prepare to add tokens to the parse tree

--- src/parse.ts
+++ src/parse.ts
@@ -1,14 +1,37 @@
 import checkmm from 'checkmm';
 import { TokenArray } from 'checkmm/dist/tokens';
 
+interface TokenArrayEvents {
+    onToken(token: string): boolean;
+    onPop(token: string): void;
+}
+
 class MonitoredTokenArray extends TokenArray {
-    constructor(private onPop: (token: string) => void) {
+    constructor(private events: TokenArrayEvents) {
         super();
     }
+    handleDiscards() {
+        for (;;) {
+            const token = super.front();
+            if (token !== undefined) {
+                const useToken = this.events.onToken(token);
+                if (!useToken) {
+                    super.pop();
+                    continue;
+                }
+            }
+            return token;
+        }
+    }
+    front() {
+        this.handleDiscards();
+        return super.front();
+    }
     pop() {
+        this.handleDiscards();
         const token = super.pop();
         if (token !== undefined) {
-            this.onPop(token);
+            this.events.onPop(token);
         }
         return token;
     }
@@ -16,8 +39,22 @@ class MonitoredTokenArray extends TokenArray {
 
 export const parse = (text: string) => {
     const parseTree: string[] = [];
+    const comments: string[] = [];
 
-    checkmm.tokens = new MonitoredTokenArray(token => parseTree.push(token));
+    const readcomment = checkmm.readcomment;
+
+    checkmm.readcomment = () => {
+        const comment = readcomment();
+        comments.push(comment);
+        checkmm.tokens.push('$(');
+        return comment;
+    };
+
+    checkmm.tokens = new MonitoredTokenArray({
+        onToken: token => token !== '$(',
+        onPop: token => parseTree.push(token),
+    });
+
     checkmm.data = text;
 
     while (checkmm.readtokenstofileinclusion()) {}
