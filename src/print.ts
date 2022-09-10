ca139e6ae973330f8fbb4b260e399f71e800f3e4
Antony Bartlett <antony.bartlett@ukri.org>
2022-09-10 09:44:18 +0100

    Support printing of other node types

--- src/print.ts
+++ src/print.ts
@@ -1,5 +1,5 @@
 import prettier from 'prettier';
-import { MMComment, MMNodeC, MMNodeLabel, MMNodeMM, MMNodeScope, MMNodeV } from './parseTreeFormat';
+import { MMComment, MMNode, MMNodeC, MMNodeLabel, MMNodeMM, MMNodeScope, MMNodeV } from './parseTreeFormat';
 
 const { group, hardline, join, line } = prettier.doc.builders;
 
@@ -71,14 +71,33 @@ const printmm = (node: MMNodeMM): prettier.Doc => {
     return printScopeChildren(node);
 };
 
-export const print = (ast: prettier.AstPath<MMNodeMM>, options: prettier.ParserOptions): string => {
+export const print = (ast: prettier.AstPath<MMNode>, options: prettier.ParserOptions): string => {
     const rootNode = ast.getValue();
 
     if (rootNode === null) {
         throw new Error('No node');
     }
 
-    const doc = printmm(rootNode);
+    let doc: prettier.Doc;
+
+    switch (rootNode.type) {
+        case 'root':
+            doc = printmm(rootNode);
+            break;
+        case '${':
+            doc = printscope(rootNode);
+            break;
+        case '$c':
+            doc = printc(rootNode);
+            break;
+        case '$v':
+            doc = printv(rootNode);
+            break;
+        case 'label':
+            doc = printlabel(rootNode);
+            break;
+    }
+
     const output = prettier.doc.printer.printDocToString(doc, options).formatted;
     return output;
 };
